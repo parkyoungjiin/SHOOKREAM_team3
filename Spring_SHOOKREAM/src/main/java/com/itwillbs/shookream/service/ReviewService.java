0efd279bba898222209841c5e6f1865a057efe38
taehun6884 <112138204+taehun6884@users.noreply.github.com>
2023-02-01 11:33:05 +0900

    02021
    asdsad

---
+++ Spring_SHOOKREAM/src/main/java/com/itwillbs/shookream/service/ReviewService.java
@@ -0,0 +1,15 @@
+package com.itwillbs.shookream.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itwillbs.shookream.mapper.MainMapper;
+import com.itwillbs.shookream.vo.ProductBean;
+
+@Service
+public class ReviewService {
+	
+	
+}
