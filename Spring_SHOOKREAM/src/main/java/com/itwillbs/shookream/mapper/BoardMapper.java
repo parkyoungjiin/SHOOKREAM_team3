499d336b15769af6170e10c8cc1de613ad7761aa
kibaekk <115527219+kibaekk@users.noreply.github.com>
2023-03-01 18:17:05 +0900

    주석처리 + 쿠키삭제

--- Spring_SHOOKREAM/src/main/java/com/itwillbs/shookream/mapper/BoardMapper.java
+++ Spring_SHOOKREAM/src/main/java/com/itwillbs/shookream/mapper/BoardMapper.java
@@ -11,21 +11,22 @@ import com.itwillbs.shookream.vo.ProductVo;
 @Mapper
 public interface BoardMapper {
 
-		
+		//게시판 등록작업
 		int insertBoard(BoardVo board);
-
+		//공지 목록
 		List<BoardVo> selectBoardList(@Param("keyword") String keyword,@Param("startRow") int startRow, @Param("listLimit")int listLimit, @Param("notice_type") String type);
-
+		//공지 목록 번호 
 		int selectBoardListCount(@Param("keyword")String keyword,@Param("notice_type") String notice_type);
-
+		//공지 상세정보 및 수정 
 		BoardVo selectBoard(int notice_idx);
-
-		BoardVo selectModifyBoard(int notice_idx);
-
+		
+		//공지 상세정보 & 수정 & 자주묻는질문 상세 
 		int updateReadcount(int notice_idx);
-
+		//공지 수정 pro
 		int updateBoard(BoardVo board);
-
+		//공지 삭제 
 		boolean deleteBoard(int notice_idx);
+		
+//		BoardVo selectModifyBoard(int notice_idx);
 
 }
