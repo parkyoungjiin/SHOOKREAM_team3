4da109b96514102f55429e215b861120f269e529
kibaekk <115527219+kibaekk@users.noreply.github.com>
2023-03-13 03:26:45 +0900

    김기백 주석
    123

--- Spring_SHOOKREAM/src/main/java/com/itwillbs/shookream/controller/ProductController.java
+++ Spring_SHOOKREAM/src/main/java/com/itwillbs/shookream/controller/ProductController.java
@@ -82,8 +82,12 @@ public class ProductController {
 		image = service.getImage(product_idx);
 		System.out.println("image 조회 : " + image);
 		
+		session.setAttribute("product", product);
+		session.setAttribute("image", image);
+		
 		model.addAttribute("product", product);
 		model.addAttribute("image", image);
+		
 		// ======================================================
 		
 		
@@ -96,6 +100,7 @@ public class ProductController {
 		//이미지 리스트 출력
 		List<imageVo> imagelist = service.getImageList(product.getProduct_name());
 		System.out.println(imagelist);
+		
 		
 		model.addAttribute("categorylist", categorylist);
 		model.addAttribute("colorlist", colorlist);
